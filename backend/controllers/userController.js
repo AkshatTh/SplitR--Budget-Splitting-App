729cb2c601d39c17cc779782c85d131df9c9fbfd
AkshatTh <akshatt.th@gmail.com>
2025-12-13 14:48:09 +0530

    Creating Backend (Implemented User authentication Middleware)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,6 +61,11 @@ const loginUser = asyncHandler(async (req, res) => {
 
 
 
+const getMe = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user);
+});
+
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d', // Token works for 30 days
@@ -70,4 +75,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 };
