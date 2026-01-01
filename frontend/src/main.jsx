ba4441f4e3992428d363afbf35555d42ee580317
AkshatTh <akshatt.th@gmail.com>
2026-01-01 22:14:50 +0530

    Fix: Set global Axios base URL for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
+else {
+ axios.defaults.baseURL = 'http://localhost:5000';
+}
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
